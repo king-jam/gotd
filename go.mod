7176cef0bccc741ce8eebdc49c62d5fb6cd892dd
James King <james.king@emc.com>
2019-05-17 10:21:02 -0400

    Migrated go-pg to GORM
    Signed-off-by: Minh Nguyen <Minh.Nguyen4@emc.com>

--- go.mod
+++ go.mod
@@ -3,21 +3,13 @@ module github.com/king-jam/gotd
 go 1.12
 
 require (
-	github.com/alexbyk/ftest v1.0.2 // indirect
-	github.com/alexbyk/panicif v1.0.2
-	github.com/go-pg/pg v8.0.4+incompatible
 	github.com/gorilla/websocket v1.4.0 // indirect
-	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/jinzhu/gorm v1.9.8
 	github.com/lusis/go-slackbot v0.0.0-20180109053408-401027ccfef5 // indirect
 	github.com/lusis/slack-test v0.0.0-20190426140909-c40012f20018 // indirect
 	github.com/nlopes/slack v0.5.0
-	github.com/onsi/ginkgo v1.8.0 // indirect
-	github.com/onsi/gomega v1.5.0 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/stretchr/testify v1.3.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	mellium.im/sasl v0.0.0-00010101000000-000000000000 // indirect
 )
 
 replace mellium.im/sasl => github.com/mellium/sasl v0.2.1
--- postgres/client.go
+++ postgres/client.go
@@ -1,57 +1,74 @@
 package postgres
 
 import (
+	"errors"
 	"fmt"
 	"net/url"
 
-	"github.com/alexbyk/panicif"
-	"github.com/go-pg/pg"
-	"github.com/go-pg/pg/orm"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres" // per gorm
 )
 
+var (
+	// ErrRecordNotFound record not found error, happens when haven't find any matched data when looking up with a struct
+	ErrRecordNotFound = errors.New("record not found")
+)
+
+// ErrDatabaseGeneral is a generic error wrapper for unexplained errors
+type ErrDatabaseGeneral string
+
+func (edg ErrDatabaseGeneral) Error() string {
+	return fmt.Sprintf("General Database Error: %s", edg)
+}
+
 type DBClient struct {
-	database *pg.DB
+	db *gorm.DB
 }
 
 type GOTD struct {
-	ID  int
+	gorm.Model
 	GIF string
 }
 
 // InitDatabase takes a connection string URL to pass into the Database
 func InitDatabase(url *url.URL) (*DBClient, error) {
-	options, err := pg.ParseURL(url.String())
+	db, err := gorm.Open(url.Scheme, url.String())
 	if err != nil {
-		return nil, fmt.Errorf("Failure to parse opts: %s", err)
+		return nil, err
 	}
 
-	pgdb := pg.Connect(options)
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	db.DB().SetMaxIdleConns(20)
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	db.DB().SetMaxOpenConns(20)
 
-	// if !db.HasTable(&TokenData{}) {
-	// 	db.CreateTable(&TokenData{})
-	// }
-
-	err = pgdb.CreateTable(&GOTD{}, &orm.CreateTableOptions{
-		Temp:          false, // create temp table
-		FKConstraints: false,
-		IfNotExists:   true,
-	})
-	panicif.Err(err)
+	if !db.HasTable(&GOTD{}) {
+		db.CreateTable(&GOTD{})
+	}
 
 	return &DBClient{
-		database: pgdb,
+		db: db,
 	}, nil
 }
 
 func (c *DBClient) Insert(gif *GOTD) error {
-	err := c.database.Insert(gif)
-	if err != nil {
-		return err
+	if result := c.db.Create(gif); result.Error != nil {
+		return ErrDatabaseGeneral(result.Error.Error())
+	}
+	return nil
+}
+
+func (c *DBClient) Update(gif *GOTD) error {
+	if result := c.db.Model(&GOTD{}).Updates(gif); result.Error != nil {
+		if gorm.IsRecordNotFoundError(result.Error) {
+			return ErrRecordNotFound
+		}
+		return ErrDatabaseGeneral(result.Error.Error())
 	}
 	return nil
 }
 
 // Close wraps the db close function for easy cleanup
 func (c *DBClient) Close() {
-	c.database.Close()
+	c.db.Close()
 }
