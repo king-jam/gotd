001dc704686856720563c7759fa4583f98cfe64a
James King <james.king@emc.com>
2019-09-30 06:59:41 -0400

    Added some Cambridge folks

--- slack_integration/users.go
+++ slack_integration/users.go
@@ -1,27 +1,33 @@
 package slack_integration
 
 var UserIdList = []string{
-	"U5SFY08HW", // Ethan
-	"U5SFZ590Q", // Val
-	"UGG0Y2W82", // Aman
-	"U5UAGKX4L", // Amy
-	"U5U133V3Q", // Geoff
-	"U5U0X61DM", // Joe
-	"U5U1DSEQ7", // Justin
-	"U61HFJ7V2", // Kranti
-	"UFJRQ2S2F", // Minh
-	"UFDAJLGJU", // Viet
-	"U5V5T2DPZ", // Dale
-	"UGYDW6UJK", // Edgardo
-	"U5T9HLMAN", // James King
-	"UEK11RZJP", // Sammie
-	"UFQQU5S7N", // Nicole
-	"U5Y4FJ8JK", // Erin Bournival
-	"UM4E99TM4", // Jim Censabella
-	"UKUAY9URK", // Jeff Estrada
-	"UKWLQ33L3", // Charles Williams
-	"UK4UMCQDV", // Calvin
-	"UM9GXDZ5E", // Brett
+	// Hopkinton
+	"U5SFY08HW", // Ethan K
+	"U5SFZ590Q", // Val C
+	"UGG0Y2W82", // Aman W
+	"U5UAGKX4L", // Amy M
+	"U5U133V3Q", // Geoff R
+	"U5U0X61DM", // Joe G
+	"U5U1DSEQ7", // Justin K
+	"U61HFJ7V2", // Kranti U
+	"UFJRQ2S2F", // Minh N
+	"UFDAJLGJU", // Viet D
+	"U5V5T2DPZ", // Dale B
+	"UGYDW6UJK", // Edgardo R
+	"U5T9HLMAN", // James K
+	"UEK11RZJP", // Sammie G
+	"UFQQU5S7N", // Nicole R
+	"U5Y4FJ8JK", // Erin B
+	"UM4E99TM4", // Jim C
+	"UKUAY9URK", // Jeff E
+	"UKWLQ33L3", // Charles W
+	"UK4UMCQDV", // Calvin C
+	"UM9GXDZ5E", // Brett B
+	// Cambridge
+	"UDDRDQ5LH", // Amy S
+	"U5SFUMV32", // Megan M
+	"U74S52CLT", // Thinh N
+	"U6VF3D8AW", // Xhe
 }
 
 // validateUser will validate userID against the UserIdList
