69de2b4be4f1cc1c98a172b41f19d0995755ea33
Minh Nguyen <Minh.Nguyen4@emc.com>
2019-12-12 09:46:51 -0500

    Update user ID for slack migration

--- pkg/slack/users.go
+++ pkg/slack/users.go
@@ -4,33 +4,33 @@ package slack
 func (h slashCommandHandler) validateUser(userID string) bool {
 	var userIDList = []string{
 		// Hopkinton
-		"U5SFY08HW", // Ethan K
-		"U5SFZ590Q", // Val C
-		"UGG0Y2W82", // Aman W
-		"U5UAGKX4L", // Amy M
-		"U5U133V3Q", // Geoff R
-		"U5U0X61DM", // Joe G
-		"U5U1DSEQ7", // Justin K
-		"U61HFJ7V2", // Kranti U
+		"WR3R1N810", // Ethan K
+		"WRGJMAYGG", // Val C
+		"WRG57T0QL", // Aman W
+		"WR3R1UBT4", // Amy M
+		"WR953FV3K", // Geoff R
+		"WRG56TZCL", // Joe G
+		"WRG56U7RS", // Justin K
+		"WRGJMR8LQ", // Kranti U
 		"WRJEJEEF9", // Minh N
-		"UFDAJLGJU", // Viet D
-		"U5V5T2DPZ", // Dale B
-		"UGYDW6UJK", // Edgardo R
-		"U5T9HLMAN", // James K
-		"UEK11RZJP", // Sammie G
-		"UFQQU5S7N", // Nicole R
-		"U5Y4FJ8JK", // Erin B
-		"UM4E99TM4", // Jim C
-		"UKUAY9URK", // Jeff E
-		"UKWLQ33L3", // Charles W
-		"UK4UMCQDV", // Calvin C
-		"UM9GXDZ5E", // Brett B
+		"WR3R2QZM0", // Viet D
+		"WRG570H1A", // Dale B
+		"WR8LBQQM7", // Edgardo R
+		"WR8LAPMKK", // James K
+		"WR8LBN249", // Sammie G
+		"WR8LBPNV7", // Nicole R
+		"WR54WA943", // Erin B
+		"WRK98THJS", // Jim C
+		"WRGJYNQ7P", // Jeff E
+		"WRG5816CU", // Charles W
+		"WR54X6ZKM", // Calvin C
+		"WLYG93DAR", // Brett B
 		// Cambridge
-		"UDDRDQ5LH", // Amy S
-		"U5SFUMV32", // Megan M
-		"U74S52CLT", // Thinh N
-		"U6VF3D8AW", // Xhe
-		"UBFRHV5GW", // AK
+		"WRG57PEN8", // Amy S
+		"WRG56LXEG", // Megan M
+		"WLYC0PC2Y", // Thinh N
+		"WM0MKG77H", // Xhe
+		"WRBHB0JP9", // AK
 	}
 
 	for _, user := range userIDList {
